8ff21ea5db862577123541ec8344fc2335afc45f
honeypigman <honeypigman@gmail.com>
2021-03-25 03:45:16 +0900

    20210325-Add-GoogleAnalystic

--- resources/views/api/layout/header.blade.php
+++ resources/views/api/layout/header.blade.php
@@ -4,7 +4,7 @@
 //-->
 <!doctype html>
 <html lang="ko">
-  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <head>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <meta charset="utf-8">
@@ -14,16 +14,20 @@
     <script src="/js/jquery.min.js"></script>
     <link href="{{ mix('/css/common.css') }}" rel="stylesheet">
     <link href="/css/flip.css" rel="stylesheet">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">{{ env('APP_NAME') }}</a>
-      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-      <!-- <span class="text-start text-white w-100 ps-2"> {{ env('API_TITLE') }} <a href="{{ env('API_LINK') }}" target="_blank" class="text-white"}><span data-feather="external-link" style="cursor:pointer;"></span></a></span>      -->
-  </header>
-
-  @include('api/layout/nav')
+    @if(env('APP_ENV') == 'P')
+      @include('cmm.googletagmanager')
+    @endif
+  </head>
 
   <body class="text-center">
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">{{ env('APP_NAME') }}</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </header>
+
+    @include('api/layout/nav')
 
     @if(Request::segment(1)=='map')
       @yield('content')

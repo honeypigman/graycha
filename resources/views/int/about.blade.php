b3ba0308a092573fff846774b6e87a76d7388d9c
honeypigman <honeypigman@gmail.com>
2021-03-25 23:14:02 +0900

    20210325-graycha-aboutme-update

--- resources/views/int/about.blade.php
+++ resources/views/int/about.blade.php
@@ -9,9 +9,12 @@
             <h4 class="text-white">Contact</h4>
             <ul class="list-unstyled">
                 <li>
-                <a href="{{ env('LINK_GITHUB') }}" target="_blank" class="text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github "><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-                </a>
+                    <a href="{{ env('LINK_GITHUB') }}" target="_blank" class="text-white sns-icons">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github "><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+                    </a>
+                    <a href="{{ env('LINK_BLOG') }}" target="_blank" class="text-white sns-icons">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-type"><polyline points="4 7 4 4 20 4 20 7"></polyline><line x1="9" y1="20" x2="15" y2="20"></line><line x1="12" y1="4" x2="12" y2="20"></line></svg>
+                    </a>                
                 </li>
             </ul>
             </div>
--- resources/views/int/section.blade.php
+++ resources/views/int/section.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
       <div class="col-lg-7 col-md-8 mx-auto">
         <h1 class="fw-bold">Hello World</h1>
-        <p class="lead text-muted">개인 프로젝트와 아이디어를 기록하는 공간입니다.</p>
+        <p class="lead text-muted">개인 프로젝트와 생각을 기록하는 공간입니다.</p>
       </div>
     </div>
 </section>

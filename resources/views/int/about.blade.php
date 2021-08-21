2b78fe6fdf24b91aa4f31926ffab6a724f45e85f
honeypigman <honeypigman@gmail.com>
2021-08-22 01:13:16 +0900

    20210821-Blper Service Release Update

--- resources/views/int/about.blade.php
+++ resources/views/int/about.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-sm-8 col-md-7 py-4">
             <h4 class="text-white">About Me</h4>
-            <p class="text-muted">오늘 하루도 배우고 성장할 수 있음에 감사하자.<br/>느리지만 꾸준함을 목표로하는 평범한 개발자.</p>
+            <p class="text-muted">오늘 하루도 배우고 성장할 수 있음에 감사하자<br/>느리지만 꾸준함을 목표로하는 평범한 개발자</p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
             <h4 class="text-white">Contact</h4>
